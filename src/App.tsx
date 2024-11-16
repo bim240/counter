77897f9e793f3fdcfe09ac0b9e0a806d94b4f1d8
bim240 <bimlendu357@gmail.com>
2024-11-16 13:50:41 +0530

    add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './styles.module.css'
 
 function App() {
+  const [counter, setCounter] = React.useState<number>(0);
+  const [stop, setStop] = React.useState<boolean| null >(null);
+  var counterIntervalId: string | number | NodeJS.Timer | undefined;
+
+  React.useEffect(() => {
+    // eslint-disable-next-line eqeqeq
+    if (stop == false) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      counterIntervalId = setInterval(() => setCounter((pre) => pre + 1), 1000);
+    }
+
+    return () => clearInterval(counterIntervalId);
+  });
+
+  const submitHandler = () => setStop(false);
+
+  const resetHandler = () => {
+    setCounter(0);
+    setStop(true);
+  };
+  const stopHandler = () => {
+    setStop(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <p className={classes.counter}>{counter}</p>
+
+      <button onClick={stop === null || stop ? submitHandler : stopHandler}>{ stop === null || stop? "Start": 'Pause'}</button>
+      {/* <button onClick={stopHandler}>Stop</button> */}
+      <button onClick={resetHandler}>Reset</button>
     </div>
   );
 }
