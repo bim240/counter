da2248b7187fa454d2e27874679997965ea3fc50
bim240 <bimlendu357@gmail.com>
2024-11-16 13:56:06 +0530

    add eslint, prettier and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
+import React from 'react'
 import classes from './styles.module.css'
 
 function App() {
-  const [counter, setCounter] = React.useState<number>(0);
-  const [stop, setStop] = React.useState<boolean| null >(null);
-  var counterIntervalId: string | number | NodeJS.Timer | undefined;
+  const [counter, setCounter] = React.useState<number>(0)
+  const [stop, setStop] = React.useState<boolean | null>(null)
+  var counterIntervalId: string | number | NodeJS.Timer | undefined
 
   React.useEffect(() => {
     // eslint-disable-next-line eqeqeq
     if (stop == false) {
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      counterIntervalId = setInterval(() => setCounter((pre) => pre + 1), 1000);
+      counterIntervalId = setInterval(() => setCounter(pre => pre + 1), 1000)
     }
 
-    return () => clearInterval(counterIntervalId);
-  });
+    return () => clearInterval(counterIntervalId)
+  })
 
-  const submitHandler = () => setStop(false);
+  const submitHandler = () => setStop(false)
 
   const resetHandler = () => {
-    setCounter(0);
-    setStop(true);
-  };
+    setCounter(0)
+    setStop(true)
+  }
   const stopHandler = () => {
-    setStop(true);
-  };
+    setStop(true)
+  }
 
   return (
     <div className={classes.app}>
       <p className={classes.counter}>{counter}</p>
 
-      <button onClick={stop === null || stop ? submitHandler : stopHandler}>{ stop === null || stop? "Start": 'Pause'}</button>
+      <button onClick={stop === null || stop ? submitHandler : stopHandler}>
+        {stop === null || stop ? 'Start' : 'Pause'}
+      </button>
       {/* <button onClick={stopHandler}>Stop</button> */}
       <button onClick={resetHandler}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
